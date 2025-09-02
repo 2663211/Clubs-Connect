f7204378cd363d86c6f6050dbb66ffc8be07d1a2
Busisiwe Mnisi <busisiwemnisi753@gmail.com>
2025-09-02 14:37:39 +0200

    doc: fix merge commit

--- terrestrial-trappist/astro.config.mjs
+++ terrestrial-trappist/astro.config.mjs
@@ -4,7 +4,6 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
-<<<<<<< Updated upstream
 	integrations: [
 		starlight({
 			title: 'Bit-by-Bit',
@@ -98,93 +97,5 @@ export default defineConfig({
 	],
 	site: 'https://thetomaunatlala-777.github.io',
 	base: '/Clubs-Connect/',
-=======
-  integrations: [
-    starlight({
-      title: 'Bit-by-Bit',
-      logo: {
-        light: './src/assets/logo_black.png',
-        dark: './src/assets/logo_white.png',
-        replacesTitle: true,
-      },
-      social: [
-        {
-          icon: 'github',
-          label: 'GitHub',
-          href: 'https://github.com/2663211/Clubs-Connect.git',
-        },
-      ],
-      customCss: ['./src/styles.css'],
-      sidebar: [
-        {
-          label: 'Home',
-          slug: 'index',
-        },
-        {
-          label: 'Introduction',
-          items: [
-            { label: 'The Bit-by-Bit Team', slug: 'introduction/team' },
-            { label: 'Project Overview', slug: 'introduction/clubs_connect' },
-          ],
-        },
-        {
-          label: 'Planning',
-          items: [
-            { label: 'Methodology', slug: 'planning/methodology' },
-            //{ label: 'Project Overview', slug: 'introduction/clubs_connect' }
-          ],
-        },
-        {
-          label: 'Analysis',
-          items: [
-            { label: 'User Strories', slug: 'analysis/user_stories' },
-            //{ label: 'Project Overview', slug: 'introduction/clubs_connect' }
-          ],
-        },
-        {
-          label: 'Design',
-          items: [
-            { label: 'The Bit-by-Bit Team', slug: 'introduction/team' },
-            { label: 'Project Overview', slug: 'introduction/clubs_connect' },
-          ],
-        },
-        {
-          label: 'Development',
-          items: [
-            { label: 'SCRUM Ceremonies', slug: 'development/scrum_ceremonies' },
-            { label: 'Project Overview', slug: 'introduction/clubs_connect' },
-          ],
-        },
-        {
-          label: 'Deployment',
-          items: [
-            { label: 'The Bit-by-Bit Team', slug: 'introduction/team' },
-            { label: 'Project Overview', slug: 'introduction/clubs_connect' },
-          ],
-        },
-        {
-          label: 'Maintanance',
-          items: [
-            { label: 'The Bit-by-Bit Team', slug: 'introduction/team' },
-            { label: 'Project Overview', slug: 'introduction/clubs_connect' },
-          ],
-        },
-        {
-          label: 'LLMs Usage',
-          slug: 'llms',
-        },
-        {
-          label: 'Reference',
-          slug: 'references',
-        },
-        {
-          label: 'Appendix',
-          slug: 'appendix',
-        },
-      ],
-    }),
-  ],
-  site: 'https://github.com/2663211/Clubs-Connect.git',
-  base: '/Clubs-Connect/',
->>>>>>> Stashed changes
+
 });
