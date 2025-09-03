e340685e5731985f0b4726604d67f0825dfbe628
Pabi Moloantoa <paballomoloantoa3858@gmail.com>
2025-09-03 14:11:22 +0200

    main to my branch

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,26 +1,16 @@
-<<<<<<< HEAD
 import React from "react";
 import "../styles/Card.css";
 import props from "prop-types";
-=======
-import React from 'react';
-import '../styles/Card.css';
-import props from 'prop-types';
->>>>>>> origin/main
 
 export default function Card(props) {
   return (
     <section className="card">
       <section className="card-content">
-<<<<<<< HEAD
         <img
           src={props.image}
           alt="image describing content under"
           className="card-image"
         />
-=======
-        <img src={props.image} alt="Club logo" className="card-image" />
->>>>>>> origin/main
         <p className="card-description">{props.description}</p>
       </section>
     </section>
