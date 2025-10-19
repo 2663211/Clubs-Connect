705162ecbf61eb757499fa2999847372100500ef
Busisiwe Mnisi <busisiwemnisi753@gmail.com>
2025-10-19 12:09:22 +0200

    changed logo & security stuff

---
+++ .astro/content-assets.mjs
@@ -0,0 +1,4 @@
+
+import __ASTRO_IMAGE_IMPORT_2mf6ln from "../../assets/network_white.png?astroContentImageFlag=&importer=src%2Fcontent%2Fdocs%2Findex.mdx";
+export default new Map([["../../assets/network_white.png?astroContentImageFlag=&importer=src%2Fcontent%2Fdocs%2Findex.mdx", __ASTRO_IMAGE_IMPORT_2mf6ln]]);
+		
---
+++ .astro/content-modules.mjs
@@ -0,0 +1,6 @@
+
+export default new Map([
+["src/content/docs/index.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Findex.mdx&astroContentModuleFlag=true")],
+["src/content/docs/introduction/team.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fintroduction%2Fteam.mdx&astroContentModuleFlag=true")],
+["src/content/docs/maintanance/user_guide.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fmaintanance%2Fuser_guide.mdx&astroContentModuleFlag=true")]]);
+		
